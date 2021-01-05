3e5879177a54ea1628891e08d2f77219a20ab5ac
hemsleek <asiyanbimubashir@gmail.com>
2021-01-05 14:50:59 +0100

    feat: coming soon page

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    colors:{
+      "mygreen":"#1B734F",
+      "myorange":"#F09330"
+    }
   },
   variants: {
     extend: {},
